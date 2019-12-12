c04531ff62c6e67e665edd7a05202f00e88b4002
Pedro Miguel <pedro.miguel@live.co.uk>
2019-12-12 22:53:08 +0000

    Test components

---
+++ app/tests/components/Todo.test.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import expect from 'expect';
+
+import Todo from 'Todo';
+
+describe('Todo', () => {
+  it('should exist', () => {
+    expect(Todo).toBeTruthy();
+  });
+});
---
+++ app/tests/components/TodoList.test.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import expect from 'expect';
+
+import TodoList from 'TodoList';
+
+describe('TodoList', () => {
+  it('should exist', () => {
+    expect(TodoList).toBeTruthy();
+  });
+});
