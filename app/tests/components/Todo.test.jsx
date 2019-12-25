7f1418c9fb2966e190df2bba1bcb0e874175523c
Pedro Miguel <pedro.miguel@live.co.uk>
2019-12-25 19:40:51 +0100

    Update tests

--- app/tests/components/Todo.test.jsx
+++ app/tests/components/Todo.test.jsx
@@ -1,10 +1,33 @@
 import React from 'react';
-import expect from 'expect';
+import ReactDOM from 'react-dom';
+import ReactTestUtils from 'react-dom/test-utils';
+import $ from 'jquery';
+import sinon from 'sinon';
+import { assert, expect } from 'chai';
 
 import Todo from 'Todo';
 
 describe('Todo', () => {
+  afterEach(() => {
+    // restore the default sandbox here
+    sinon.restore();
+  });
+
   it('should exist', () => {
-    expect(Todo).toBeTruthy();
+    expect(Todo).to.exist;
+  });
+
+  it('should call onToggle prop with id on click', () => {
+    const todoData = {
+      'id': 199,
+      'text': 'Process accounting receipts',
+      'completed': true,
+    };
+    const spy = sinon.spy();
+    const todo = ReactTestUtils.renderIntoDocument(<Todo {...todoData} onToggle={spy} />);
+    const $el = $(ReactDOM.findDOMNode(todo));
+
+    ReactTestUtils.Simulate.click($el[0]);
+    assert(spy.calledWith(todoData.id));
   });
 });
--- app/tests/components/TodoApp.test.jsx
+++ app/tests/components/TodoApp.test.jsx
@@ -18,4 +18,19 @@ describe('TodoApp', () => {
 
     expect(todoApp.state.todos[0].text).to.equal(todoText);
   });
+
+  it('should toggle completed value when handleToggle called', () => {
+    const todoData = {
+      'id': 11,
+      'text': 'Test features',
+      'completed': false,
+    };
+    const todoApp = ReactTestUtils.renderIntoDocument(<TodoApp />);
+    todoApp.setState({ 'todos': [todoData] });
+
+    expect(todoApp.state.todos[0].completed).to.be.false;
+
+    todoApp.handleToggle(todoData.id);
+    expect(todoApp.state.todos[0].completed).to.be.true;
+  });
 });
