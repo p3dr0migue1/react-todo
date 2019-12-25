d8908359cceeb30cf3cb49240417fb1b4d3521ce
Pedro Miguel <pedro.miguel@live.co.uk>
2019-12-25 11:44:13 +0000

    Test search component should exist

---
+++ app/tests/components/TodoSearch.test.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import expect from 'expect';
+
+// local imports
+import TodoSearch from 'TodoSearch';
+
+describe('TodoSearch', () => {
+  it('should exist', () => {
+    expect(TodoSearch).toBeTruthy();
+  });
+});
