cd7a61613f81c629e67612a0894129a0c910d9ae
Pedro Miguel <pedro.miguel@live.co.uk>
2019-12-25 12:55:28 +0000

    Update tests

--- app/tests/components/TodoApp.test.jsx
+++ app/tests/components/TodoApp.test.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
-import expect from 'expect';
+import ReactTestUtils from 'react-dom/test-utils';
+import { expect } from 'chai';
 
 import TodoApp from 'TodoApp';
 
 describe('TodoApp', () => {
   it('should exist', () => {
-    expect(TodoApp).toBeTruthy();
+    expect(TodoApp).to.exist;
+  });
+
+  it('should add todo to the todos state on handleAddTodo', () => {
+    const todoText = 'Book concert tickets';
+    const todoApp = ReactTestUtils.renderIntoDocument(<TodoApp />);
+
+    todoApp.setState({ todos: [] });
+    todoApp.handleAddTodo(todoText);
+
+    expect(todoApp.state.todos[0].text).to.equal(todoText);
   });
 });
